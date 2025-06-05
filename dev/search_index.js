b6e02ca98fd9d64044f83b9a31993c98d11ffb34
Documenter.jl <documenter@juliadocs.github.io>
2025-06-05 21:46:10 +0000

    build based on c7830b4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MatrixBandwidth","page":"Home","title":"MatrixBandwidth","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MatrixBandwidth.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
